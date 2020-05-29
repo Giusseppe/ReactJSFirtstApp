b807c62e6b3457904437fd8a6c54156c150ddd3a
Yupee <gi.velizr@gmail.com>
2020-05-29 18:38:11 -0400

    paso props entre componentes basicos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HolaMundo from "./components/HolaMundo";
-import AdiosMundo from "./components/AdiosMundo";
+import Saludar from "./components/Saludar";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HolaMundo />
-        <AdiosMundo />
+        <Saludar name="Giusseppe" edad="28" />
+        <Saludar name="Batman" edad="39" />
       </header>
     </div>
   );
