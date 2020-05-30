29c9766923de6423fb8aeafdb44cc25f8fd1d389
Yupee <gi.velizr@gmail.com>
2020-05-29 20:00:03 -0400

    hook de estado - useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-// import logo from "./logo.svg";
+import React, { useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 // import Saludar from "./components/Saludar";
-import { Button, Accordion, Card, Alert } from "react-bootstrap";
+// import { Button, Accordion, Card, Alert } from "react-bootstrap";
 
-import { ReactComponent as ReactIcon } from "./assets/original.svg";
+// import { ReactComponent as ReactIcon } from "./assets/original.svg";
 
 function App() {
   // const userInfo = { name: "Giusseppe", edad: 28, color: "Azul" };
@@ -13,47 +13,53 @@ function App() {
   // const saludarFn = (name, edad) => {
   //   console.log(`hola ${name} tiene ${edad} años`);
   // };
+
+  const [stateCar, setStateCar] = useState(false);
+  const encenderApagar = () => {
+    // setStateCar(!stateCar);
+    setStateCar((prevValue) => !prevValue);
+  };
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <Saludar userInfo={userInfo} saludarFn={saludarFn} />
-    //     <Saludar userInfo={userInfo2} saludarFn={saludarFn} />
-    //   </header>
-    // </div>
     <div className="App">
-      <h1>React Bootstrap</h1>
-      <Button variant="primary" onClick={() => console.log("Hola")}>
-        Click
-      </Button>{" "}
-      <Button variant="primary" size="lg" block>
-        Block level button
-      </Button>
-      <Accordion defaultActiveKey="0">
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Abrir!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! Amigo I'm the body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Abrir!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! Chorizo I'm another body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
-      <Alert variant="danger">Peligro!</Alert>
-      <ReactIcon />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3> El vehiculo esta encendido: {stateCar ? "ON" : "OFF"}</h3>
+        <button onClick={encenderApagar}>ON / OFF</button>
+      </header>
     </div>
+    // <div className="App">
+    //   <h1>React Bootstrap</h1>
+    //   <Button variant="primary" onClick={() => console.log("Hola")}>
+    //     Click
+    //   </Button>{" "}
+    //   <Button variant="primary" size="lg" block>
+    //     Block level button
+    //   </Button>
+    //   <Accordion defaultActiveKey="0">
+    //     <Card>
+    //       <Card.Header>
+    //         <Accordion.Toggle as={Button} variant="link" eventKey="0">
+    //           Abrir!
+    //         </Accordion.Toggle>
+    //       </Card.Header>
+    //       <Accordion.Collapse eventKey="0">
+    //         <Card.Body>Hello! Amigo I'm the body</Card.Body>
+    //       </Accordion.Collapse>
+    //     </Card>
+    //     <Card>
+    //       <Card.Header>
+    //         <Accordion.Toggle as={Button} variant="link" eventKey="1">
+    //           Abrir!
+    //         </Accordion.Toggle>
+    //       </Card.Header>
+    //       <Accordion.Collapse eventKey="1">
+    //         <Card.Body>Hello! Chorizo I'm another body</Card.Body>
+    //       </Accordion.Collapse>
+    //     </Card>
+    //   </Accordion>
+    //   <Alert variant="danger">Peligro!</Alert>
+    //   <ReactIcon />
+    // </div>
   );
 }
 
